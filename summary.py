7bf14f805164b7f9b70f5273e83e182f6e276cc9
icwells <smrupp16@gmail.com>
2019-05-22 15:48:16 -0700

    Sumamry now summarizes case and control records from merged file separate

--- summary.py
+++ summary.py
@@ -12,8 +12,12 @@ class Summary():
 		self.infile = infile
 		self.outfile = ""
 		self.head = None
-		self.total = 0
-		self.complete = OrderedDict()
+		self.header = "Field,Filled,Empty,%Filled,Total\n"
+		self.casecontrol = False
+		self.casetotal = 0
+		self.controltotal = 0
+		self.case = OrderedDict()
+		self.control = OrderedDict()
 		self.__setOutfile__()
 		self.__setSummary__()
 		self.__writeSummary__()
@@ -23,27 +27,45 @@ class Summary():
 		ext = getExt(self.infile)
 		path = self.infile[:self.infile.rfind(".")]
 		self.outfile = ("{}_summary.{}").format(path, ext)
+		if "mergedUCRrecords" in self.infile:
+			self.casecontrol = True
+			self.header = "Field,CaseFilled,CaseEmpty,Case%Filled,CaseTotal,ControlFilled,ControlEmpty,Control%Filled,ControlTotal\n"
+
+	def __calculateTotal__(self, n, t):
+		# Calculates full, empty, total
+		return (",{},{},{:.2%},{}").format(n, t-n, n/t, t)
 
 	def __writeSummary__(self):
 		# Write complete dict to outfile
 		with open(self.outfile, "w") as out:
-			out.write("Field,Filled,Empty,%Filled,Total\n")
-			for k in self.complete.keys():
-				n = self.complete[k]
-				e = self.total-n
-				p = n/self.total
-				out.write(("{},{},{},{:.2%},{}\n").format(k, n, e, p, self.total))
+			out.write(self.header)
+			for k in self.case.keys():
+				s = self.__calculateTotal__(self.case[k], self.casetotal)
+				if self.casecontrol == True:
+					s += self.__calculateTotal__(self.control[k], self.controltotal)
+				out.write(("{}{}\n").format(k, s))
 
 	def __setKeys__(self):
 		# Copies keys from header to complete dict
 		for k in self.head.keys():
-			self.complete[k] = 0
+			if k != "Case":
+				self.case[k] = 0
+				self.control[k] = 0
 
 	def __countRow__(self, s):
 		# Checks each column for entry
-		for k in self.complete.keys():
-			if s[self.head[k]].strip():
-				self.complete[k] += 1
+		if self.casecontrol == False or s[self.head["case"]] == "1":
+			self.casetotal += 1
+			for k in self.case.keys():
+				i = s[self.head[k]].strip()
+				if i and i != "NA":
+					self.case[k] += 1
+		else:
+			self.controltotal += 1
+			for k in self.control.keys():
+				i = s[self.head[k]].strip()
+				if i and i != "NA":
+					self.control[k] += 1
 
 	def __setSummary__(self):
 		# Counts entires per column in input file
@@ -52,7 +74,6 @@ class Summary():
 			for line in f:
 				line = line.strip()
 				if first == False:
-					self.total += 1
 					s = line.split(d)
 					self.__countRow__(s)
 				else:
