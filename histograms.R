4dc5726eb4ae7c3f673a80dc0e4472d1d47704a7
Shawn Rupp <smrupp16@gmail.com>
2019-08-29 13:13:40 -0700

    Updated histograms.R to plot nas by percent and the remaining plots with or without control records

--- histograms.R
+++ histograms.R
@@ -13,19 +13,20 @@ erBarPlot <- function(df, title, nas) {
 
 erHistogram <- function(df, title, nas) {
 	# Returns histogram of data column by er status
-	bw <- floor(max(df$Values) * 0.01)
-	return(ggplot(df, aes(Values, fill = Type)) + geom_histogram(binwidth = bw, position = "dodge") + 
+	return(ggplot(df, aes(Values, fill = Type)) + geom_histogram(bins = 100, position = "dodge") + 
            ggtitle(title))
 }
 
-getERColumns <- function(data, col, status , title) {
+getERColumns <- function(data, col, status, title) {
 	# Returns data frame of target column
 	ret <- subset(data, ER == status, select = col)
 	ret <- na.omit(ret)
 	colnames(ret) <- title
 	if (title == "ER+") {
+	  ret$"ER+" <- sapply(ret$"ER+", as.character)
 	  ret$"ER+" <- sapply(ret$"ER+", as.numeric)
 	} else {
+	  ret$"ER-" <- sapply(ret$"ER-", as.character)
 	  ret$"ER-" <- sapply(ret$"ER-", as.numeric)
 	}
 	return(ret)
@@ -36,16 +37,21 @@ getControlColumn <- function(data, col) {
 	ret <- subset(data, Case == "0", select = col)
 	ret <- na.omit(ret)
 	colnames(ret) <- "Control"
+	ret$Control <- sapply(ret$Control, as.character)
 	ret$Control <- sapply(ret$Control, as.numeric)
 	return(ret)
 }
 
-getDataFrame <- function(data, col) {
+getDataFrame <- function(data, col, controls) {
 	# Returns columns melted into dataframe
 	pos <- getERColumns(data, col, "P", "ER+")
 	neg <- getERColumns(data, col, "N", "ER-")
-	con <- getControlColumn(data, col)
-	ret <- melt(c(pos, neg, con))
+	if (controls == TRUE) {
+		con <- getControlColumn(data, col)
+		ret <- melt(c(pos, neg, con))
+	} else {
+		ret <- melt(c(pos, neg))
+	}
 	colnames(ret) <- c("Values", "Type")
 	return(ret)
 }
@@ -55,9 +61,9 @@ countNA <- function(data, col) {
 	pos <- subset(data, ER == "P", select = col)
 	neg <- subset(data, ER == "N", select = col)
 	con <- subset(data, Case == "0", select = col)
-	p <- as.numeric(colSums(is.na(pos)))
-	n <- as.numeric(colSums(is.na(neg)))
-	co<- as.numeric(colSums(is.na(con)))
+	p <- as.numeric(colSums(is.na(pos))) / nrow(pos)
+	n <- as.numeric(colSums(is.na(neg))) / nrow(neg)
+	co<- as.numeric(colSums(is.na(con))) / nrow(con)
 	ret <- data.frame(col, p, n, co)
 	colnames(ret) <- c("Field", "ER+", "ER-", "Control")
 	return(ret)
@@ -65,70 +71,58 @@ countNA <- function(data, col) {
 
 #-----------------------------------------------------------------------------
 
-plotAges <- function(data) {
-	# Saves age related plots to file
+plotAges <- function(data, controls) {
+	# Saves age related plots to list
 	ages <- list()
-	amd <- getDataFrame(data, "AgeMaD")
+	amd <- getDataFrame(data, "AgeMaD", controls)
 	ages[[1]] <- erBarPlot(amd, "Age at Mother's Death")
-	mab <- getDataFrame(data, "MaAgeBr")
+	mab <- getDataFrame(data, "MaAgeBr", controls)
 	ages[[2]] <- erBarPlot(mab, "Mother's Age at Birth")
-	apd <- getDataFrame(data, "AgePaD")
+	apd <- getDataFrame(data, "AgePaD", controls)
 	ages[[3]] <- erBarPlot(apd, "Age at Father's Death")
-	pab <- getDataFrame(data, "PaAgeBr")
+	pab <- getDataFrame(data, "PaAgeBr", controls)
 	ages[[4]] <- erBarPlot(pab, "Fathers's Age at Birth")
-	sibs <- getDataFrame(data, "NumSibsDieChildhood")
+	sibs <- getDataFrame(data, "NumSibsDieChildhood", controls)
 	ages[[5]] <- erBarPlot(sibs, "Number of Siblings Died During Childhood")
-	# Save plots to file
-	svg(file = "Z:/ELCS/histograms/ageHistograms.svg")
-	cowplot::plot_grid(plotlist = ages, nrow = 5)
-	dev.off()
+	return(ages)
 }
 
-plotSEI <- function(data) {
-	# Saves economic index plots to file
+plotSEI <- function(data, controls) {
+	# Saves economic index plots to list
 	sei <- list()
-	mnp <- getDataFrame(data, "MaCenNamPow")
+	mnp <- getDataFrame(data, "MaCenNamPow", controls)
 	sei[[1]] <- erHistogram(mnp, "Mother's Nam Powers Score")
-	msei <- getDataFrame(data, "MaCenSEI")
+	msei <- getDataFrame(data, "MaCenSEI", controls)
 	sei[[2]] <- erBarPlot(msei, "Mother's Socio-Economic Index")
-	pnp <- getDataFrame(data, "PaCenNamPow")
+	pnp <- getDataFrame(data, "PaCenNamPow", controls)
 	sei[[3]] <- erHistogram(pnp, "Fathers's Nam Powers Score")
-	psei <- getDataFrame(data, "PaCenSEI")
+	psei <- getDataFrame(data, "PaCenSEI", controls)
 	sei[[4]] <- erBarPlot(psei, "Fathers's Socio-Economic Index")
-	# Save plots to file
-	svg(file = "Z:/ELCS/histograms/seiHistograms.svg")
-	cowplot::plot_grid(plotlist = sei, nrow = 2)
-	dev.off()
+	return(sei)
 }
 
-plotIncome <- function(data) {
-	# Saves income plots to file
+plotIncome <- function(data, controls) {
+	# Saves income plots to list
 	income <- list()
-	ei <- getDataFrame(data, "EgoCenIncome")
+	ei <- getDataFrame(data, "EgoCenIncome", controls)
 	income[[1]] <- erHistogram(ei, "Ego's Income")
-	mi <- getDataFrame(data, "MaCenIncome_New")
+	mi <- getDataFrame(data, "MaCenIncome_New", controls)
 	income[[2]] <- erHistogram(mi, "Mother's Income")
-	pi <- getDataFrame(data, "PaCenIncome_New")
+	pi <- getDataFrame(data, "PaCenIncome_New", controls)
 	income[[3]] <- erHistogram(pi, "Father's Income")
-	# Save plots to file
-	svg(file = "Z:/ELCS/histograms/incomeHistograms.svg")
-	cowplot::plot_grid(plotlist = income, nrow = 3)
-	dev.off()
+	return(income)
 }
 
-plotHomeVal <- function(data) {
-	# Saves home value plots to file
+plotHomeVal <- function(data, controls) {
+	# Saves home value plots to list
 	homeval <- list()
-	ehv <- getDataFrame(data, "HomeValue1940_New")
+	ehv <- getDataFrame(data, "HomeValue1940_New", controls)
 	homeval[[1]] <- erHistogram(ehv, "Ego's 1940 Home Value")
-	mhv <- getDataFrame(data, "MaHomeValue1940_New")
+	mhv <- getDataFrame(data, "MaHomeValue1940_New", controls)
 	homeval[[2]] <- erHistogram(mhv, "Mother's 1940 Home Value")
-	phv <- getDataFrame(data, "PaHomeValue1940_New")
+	phv <- getDataFrame(data, "PaHomeValue1940_New", controls)
 	homeval[[3]] <- erHistogram(phv, "Father's 1940 Home Value")
-	# Save plots to file
-	svg(file = "Z:/ELCS/histograms/homeValHistograms.svg")
-	cowplot::plot_grid(plotlist = homeval, nrow = 3)
-	dev.off()
+	return(homeval)
 }
 
 plotNAs <- function(data) {
@@ -144,7 +138,7 @@ plotNAs <- function(data) {
 	n <- melt(nas)
 	svg(file = "Z:/ELCS/histograms/naBarPlot.svg")
 	ggplot(n, aes(x = Field, y = value, fill = variable)) + geom_bar(stat = "identity", position = "dodge") +
-		ggtitle("Adversity Fields NA Counts") + xlab("NA Count") + scale_x_discrete(nas$id) +
+		ggtitle("Adversity Fields NA Percentages") + ylab("NA Percent") + scale_x_discrete(nas$id) +
 	  theme(axis.text.x = element_text(angle = 90, hjust = 1))
 	dev.off()
 }
@@ -152,7 +146,46 @@ plotNAs <- function(data) {
 # Read csv with blanks as NAs
 data <- read.csv("Z:/ELCS/mergedUCRrecords.2019-08-07.csv", na.strings = c("", "NA"))
 
-plotAges(data)
-plotSEI(data)
-plotIncome(data)
-plotHomeVal(data)
+# Plot ages with and without controls
+ages <- plotAges(data, TRUE)
+svg(file = "Z:/ELCS/histograms/ageHistograms_controls.svg")
+cowplot::plot_grid(plotlist = ages, nrow = 5)
+dev.off()
+
+ages <- plotAges(data, FALSE)
+svg(file = "Z:/ELCS/histograms/ageHistograms_cases.svg")
+cowplot::plot_grid(plotlist = ages, nrow = 5)
+dev.off()
+
+# Plot economic status data with and without controls
+sei <- plotSEI(data, TRUE)
+svg(file = "Z:/ELCS/histograms/seiHistograms_controls.svg")
+cowplot::plot_grid(plotlist = sei, nrow = 2)
+dev.off()
+
+sei <- plotSEI(data, FALSE)
+svg(file = "Z:/ELCS/histograms/seiHistograms_cases.svg")
+cowplot::plot_grid(plotlist = sei, nrow = 2)
+dev.off()
+
+# Plot income data with and without controls
+income <- plotIncome(data, TRUE)
+svg(file = "Z:/ELCS/histograms/incomeHistograms_controls.svg")
+cowplot::plot_grid(plotlist = income, nrow = 3)
+dev.off()
+
+income <- plotIncome(data, FALSE)
+svg(file = "Z:/ELCS/histograms/incomeHistograms_cases.svg")
+cowplot::plot_grid(plotlist = income, nrow = 3)
+dev.off()
+
+# Plot home value data with and without controls
+homeval <- plotHomeVal(data, TRUE)
+svg(file = "Z:/ELCS/histograms/homeValHistograms_controls.svg")
+cowplot::plot_grid(plotlist = homeval, nrow = 3)
+dev.off()
+
+homeval <- plotHomeVal(data, FALSE)
+svg(file = "Z:/ELCS/histograms/homeValHistograms_cases.svg")
+cowplot::plot_grid(plotlist = homeval, nrow = 3)
+dev.off()
